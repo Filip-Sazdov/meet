dd2a9e9d46d9bbd93a946f508d63e9593d3bceef
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-20 00:17:58 -0500

    Add class component syntax as is in exercise.

--- src/App.js
+++ src/App.js
@@ -5,4 +5,14 @@ function App() {
 	return <div className="App"></div>;
 }
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+
+//       </div>
+//     );
+//   }
+// }
+
 export default App;
