188cc80a1282dfe271241ece93bf26504130dea3
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-20 23:36:56 -0500

    Add Event component.

---
+++ src/Event.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const Event = () => {
+	return <div></div>;
+};
+
+// class Event extends Component {
+//   render() {
+//     return <div></div>;
+//   }
+// }
+
+export default Event;
