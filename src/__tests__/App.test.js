60a1e1dd12f5c9aaa8b9e9c578315d9af4bf80e1
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-20 23:34:31 -0500

    Correct wording in test.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,7 +4,7 @@ import App from "../App";
 import EventList from "../EventList";
 
 describe("<App /> component", () => {
-	test("render list of events", () => {
+	test("should render list of events", () => {
 		const AppWrapper = shallow(<App />);
 		expect(AppWrapper.find(EventList)).toHaveLength(1);
 	});
