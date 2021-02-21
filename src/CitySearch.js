1e727c35aed760f910af369ad823c73893a4687d
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-21 00:13:37 -0500

    Add CitySearch component.

---
+++ src/CitySearch.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+const CitySearch = () => {
+	return <div className="CitySearch"></div>;
+};
+
+// class CitySearch extends Component {
+//   render() {
+//     return (
+//       <div className="CitySearch">
+//       </div>
+//     );
+//   }
+// }
+
+export default CitySearch;
