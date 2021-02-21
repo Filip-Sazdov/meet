0958d6b11ccc230d3255817b5a19c4403fbaeb55
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-20 22:11:26 -0500

    Create EventList Component.

---
+++ src/EventList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const EventList = () => {
+	return <ul className="EventList"></ul>;
+};
+
+// class EventList extends Component {
+//   render() {
+//     return (
+//       <ul className="EventList"></ul>
+//     );
+//   }
+// }
+
+export default EventList;
