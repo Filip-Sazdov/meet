86c59e9f8df854e1b6e17e3b9030376717271bc4
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-21 00:14:16 -0500

    Import CitySearch component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import CitySearch from "./CitySearch";
 import EventList from "./EventList";
 
 const App = () => {
 	return (
 		<div className="App">
+			<CitySearch />
 			<EventList />
 		</div>
 	);
