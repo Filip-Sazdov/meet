6ad77330e7bea34e614074101739ab89fccba5a9
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-20 23:35:51 -0500

    Get events as props and map each one into <li> with id.

--- src/EventList.js
+++ src/EventList.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import Event from "./Event";
 
-const EventList = () => {
-	return <ul className="EventList"></ul>;
+const EventList = ({ events }) => {
+	// const { events } = props;
+	return (
+		<ul className="EventList">
+			{events.map((event) => (
+				<li key={event.id}>
+					<Event event={event} />
+				</li>
+			))}
+		</ul>
+	);
 };
 
 // class EventList extends Component {
 //   render() {
 //     return (
-//       <ul className="EventList"></ul>
+//       <ul className="EventList">
+// 	{events.map((event) => (
+// 		<li key={event.id}>
+// 			<Event event={event} />
+// 		</li>
+// 	))}
+// </ul>
 //     );
 //   }
 // }
