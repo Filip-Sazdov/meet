6c8ac6d3fb0ed59a89dcd75e335cb1c3abc30118
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-23 02:34:21 -0500

    Add event component logic.

--- src/Event.js
+++ src/Event.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 
-const Event = () => {
-	return <div></div>;
-};
+class Event extends Component {
+	state = {
+		toggleDetails: false,
+	};
 
-// class Event extends Component {
-//   render() {
-//     return <div></div>;
-//   }
-// }
+	handleToggleDetails = () => {
+		return !this.setState.toggleDetails;
+	};
+
+	render() {
+		const { event } = this.props;
+		return (
+			<div className="event">
+				{/* Added clarifying text below as project brief visual seemed to need it. */}
+				<h1>Title of Event: {event.summary}</h1>
+				<h3>Start Time: {event.start.dateTime}</h3>
+				<h3>Location: {event.location}</h3>
+				{this.state.toggleDetails && (
+					<div>
+						<h2>About event:</h2>
+						<a href={event.htmlLink}>See Details on Google Calendar</a>
+						<p>{event.description}</p>
+					</div>
+				)}
+				<button className="toggle-details" onClick={() => this.handleToggleDetails()}>
+					Details
+				</button>
+			</div>
+		);
+	}
+}
 
 export default Event;
