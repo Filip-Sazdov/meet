c327b4b30ed228722bf582536e69952d2b183ccd
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-23 00:11:48 -0500

    Add state and events logic.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,16 +1,55 @@
+// import React, { useState } from "react";
+
+// const CitySearch = () => {
+// 	const [query, setQuery] = useState("");
+
+// 	return (
+// 		<div className="CitySearch">
+// 			<input type="text" className="city" value={query} />
+// 			<ul className="suggestions"></ul>
+// 		</div>
+// 	);
+// };
+
 import React, { Component } from "react";
 
-const CitySearch = () => {
-	return <div className="CitySearch"></div>;
-};
+class CitySearch extends Component {
+	state = {
+		query: "",
+		suggestions: [],
+	};
+	handleInputChanged = (event) => {
+		const value = event.target.value;
 
-// class CitySearch extends Component {
-//   render() {
-//     return (
-//       <div className="CitySearch">
-//       </div>
-//     );
-//   }
-// }
+		const suggestions = this.props.locations.filter((location) => {
+			return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+		});
+
+		this.setState({ query: value, suggestions });
+	};
+
+	handleItemClicked = (suggestion) => {
+		this.setState({
+			query: suggestion,
+		});
+	};
+	render() {
+		return (
+			<div className="CitySearch">
+				<input type="text" className="city" value={this.state.query} onChange={this.handleInputChanged} />
+				<ul className="suggestions">
+					{this.state.suggestions.map((suggestion) => (
+						<li key={suggestion} onClick={() => this.handleItemClicked(suggestion)}>
+							{suggestion}
+						</li>
+					))}
+					<li key="all">
+						<b>See all cities</b>
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
 
 export default CitySearch;
