a7637452cd7cdd52a9e68d58253fcdd7d6aadef6
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-02-23 02:35:41 -0500

    Add test for number of events.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import App from "../App";
 import EventList from "../EventList";
 import CitySearch from "../CitySearch";
+import NumberOfEvents from "../NumberOfEvents";
 
 describe("<App /> component", () => {
 	let AppWrapper;
@@ -14,5 +15,8 @@ describe("<App /> component", () => {
 	});
 	test("should render CitySearch", () => {
 		expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+	});
+	test("should render number of events component", () => {
+		expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
 	});
 });
