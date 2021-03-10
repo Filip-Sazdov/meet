9ac5f78a15cf5ed010c0063a73096941612a76ad
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-03-09 22:02:56 -0500

    Change heading and format date.

--- src/Event.js
+++ src/Event.js
@@ -9,13 +9,20 @@ class Event extends Component {
 		this.setState({ toggleDetails: !this.state.toggleDetails });
 	};
 
+	formatDateTime = (dateTime) => {
+		return new Intl.DateTimeFormat("en-GB", { dateStyle: "full", timeStyle: "long" }).format(dateTime);
+	};
+
 	render() {
 		const { event } = this.props;
+		const dateTime = new Date(event.start.dateTime);
+		const readabledateTime = this.formatDateTime(dateTime);
+
 		return (
 			<div className="event">
 				{/* Added clarifying text below as project brief visual seemed to need it. */}
-				<h1 className="event-title">{event.summary}</h1>
-				<p className="event-start">Start Time: {event.start.dateTime}</p>
+				<h3 className="event-title">{event.summary}</h3>
+				<p className="event-start">{readabledateTime}</p>
 				<p className="event-location">Location: {event.location}</p>
 				<button className="toggle-details" onClick={() => this.handleToggleDetails()}>
 					{!this.state.toggleDetails ? "Show Details" : "Hide Details"}
