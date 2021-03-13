6aeb3faaf0fded6ac7ca6e0a28a8ba9378dd201f
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-03-13 10:53:20 -0500

    Import EventGenre and add data-vis-wrapper.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { getEvents, extractLocations } from "./api";
 import { WarningAlert } from "./Alert";
 
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import EventGenre from "./EventGenre";
 
 class App extends Component {
 	state = {
@@ -96,23 +97,25 @@ class App extends Component {
 					updateEventCount={this.updateEventCount}
 				/>
 				<h4>Events in each city</h4>
-
-				<ResponsiveContainer height={400}>
-					<ScatterChart
-						margin={{
-							top: 20,
-							right: 20,
-							bottom: 20,
-							left: 20,
-						}}
-					>
-						<CartesianGrid />
-						<XAxis type="category" dataKey="city" name="city" />
-						<YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
-						<Tooltip cursor={{ strokeDasharray: "3 3" }} />
-						<Scatter data={this.getData()} fill="#8884d8" />
-					</ScatterChart>
-				</ResponsiveContainer>
+				<div className="data-vis-wrapper">
+					<EventGenre events={this.state.events} />
+					<ResponsiveContainer height={400}>
+						<ScatterChart
+							margin={{
+								top: 20,
+								right: 20,
+								bottom: 20,
+								left: 20,
+							}}
+						>
+							<CartesianGrid />
+							<XAxis type="category" dataKey="city" name="city" />
+							<YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+							<Tooltip cursor={{ strokeDasharray: "3 3" }} />
+							<Scatter data={this.getData()} fill="#8884d8" />
+						</ScatterChart>
+					</ResponsiveContainer>
+				</div>
 				<EventList events={this.state.events} />
 			</div>
 		);
