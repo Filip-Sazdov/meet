9751b8d59418c4fe6116b48559f615abbee37595
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-03-13 13:06:26 -0500

    Modify event object to add as prop to Modal.

--- src/Event.js
+++ src/Event.js
@@ -11,26 +11,25 @@ class Event extends Component {
 	};
 
 	formatDateTime = (dateTime) => {
-		return new Intl.DateTimeFormat("en-GB", { dateStyle: "full", timeStyle: "long" }).format(dateTime);
+		return new Intl.DateTimeFormat("en-US", { dateStyle: "full", timeStyle: "short" }).format(dateTime);
 	};
 
 	render() {
 		const { event } = this.props;
 		const dateTime = new Date(event.start.dateTime);
-		const readabledateTime = this.formatDateTime(dateTime);
+		const readableDateTime = this.formatDateTime(dateTime);
 
 		return (
 			<div className="event">
-				{/* Added clarifying text below as project brief visual seemed to need it. */}
 				<h3 className="event-title">{event.summary}</h3>
-				<p className="event-start">{readabledateTime}</p>
+				<p className="event-start">{readableDateTime}</p>
 				<p className="event-location">Location: {event.location}</p>
 				<button className="toggle-details" onClick={() => this.handleToggleDetails()}>
 					{!this.state.toggleDetails ? "Show Details" : "Hide Details"}
 				</button>
 
 				{this.state.toggleDetails && (
-					<Modal event={event} handleToggleDetails={this.handleToggleDetails} readabledateTime={readabledateTime} />
+					<Modal event={{ ...event, readableDateTime }} handleToggleDetails={this.handleToggleDetails} />
 				)}
 			</div>
 		);
